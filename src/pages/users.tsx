a40a8d6344943e9c02dbba786f59d5fcee1c1fd6
mVabo <trioward@gmail.com>
2023-09-06 11:13:22 +0200

    Updated router and main

---
+++ src/pages/users.tsx
@@ -0,0 +1,5 @@
+const UsersPage = () => {
+	return <div>users</div>;
+};
+
+export default UsersPage;
--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
+import UsersPage from './pages/users';
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <UsersPage />,
+	},
+]);
 
 export default router;
