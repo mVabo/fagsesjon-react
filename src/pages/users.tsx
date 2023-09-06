f6229d568f5d77d6bab19df4d52fcafa550c80a2
mVabo <trioward@gmail.com>
2023-09-06 11:27:42 +0200

    Added users data

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react';
+
 const UsersPage = () => {
+	const [users, setUsers] = useState<any>([]);
+
+	useEffect(() => {
+		getUsers();
+	}, []);
+
+	const getUsers = async () => {
+		const tempUsers = await fetch('https://reqres.in/api/users', {
+			method: 'GET',
+		});
+		const data = await tempUsers.json();
+		setUsers(data.data);
+	};
+
 	return (
 		<div className="w-screen h-screen flex flex-col justify-center items-center">
-			<div>users</div>
+			<h2 className="text-gray-900 text-xl font-bold">Users</h2>
+			<div>
+				{users?.map?.((user) => (
+					<div>{user.email}</div>
+				))}
+			</div>
 		</div>
 	);
 };
