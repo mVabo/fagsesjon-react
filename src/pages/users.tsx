70d513c0a4f11693266b8e7271cd9423f74e4127
mVabo <trioward@gmail.com>
2023-09-06 11:16:40 +0200

    Added basic users page

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,5 +1,9 @@
 const UsersPage = () => {
-	return <div>users</div>;
+	return (
+		<div className="w-screen h-screen flex flex-col justify-center items-center">
+			<div>users</div>
+		</div>
+	);
 };
 
 export default UsersPage;
